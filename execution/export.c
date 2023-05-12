925f3d4865b9cbf34cea542b1c0bf6a54dd46bb6
Walid Errahmani <werrahma@e1r6p8.1337.ma>
2023-05-12 19:42:04 +0100

    dup

--- execution/export.c
+++ execution/export.c
@@ -6,29 +6,75 @@
 /*   By: werrahma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:07:11 by werrahma          #+#    #+#             */
-/*   Updated: 2023/05/11 20:00:19 by werrahma         ###   ########.fr       */
+/*   Updated: 2023/05/12 18:57:07 by werrahma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	split_byequals(t_env *env, char *str)
+void	dup_value(char *str, t_env *lst)
 {
-	int	i;
+	int i;
+	int j;
 
 	i = 0;
-	// while(str[i] && str[i] != '=')
-	// {
-		env->key = ft_strdup(str - ft_strlen(str));
-	// }
-		printf("%s\n", env->key);
-		exit(1);
+
+	while(str[i] && str[i] != '=')
+		i++;
+	i++;
+	j = i;
+	while(str[j])
+		j++;
+	lst->value = malloc(j + 1);
 	while(str[i])
 	{
-		env->value = ft_strdup(str + ft_strlen(str));
+printf("%c\n", str[i]);
+		lst->value[i] = str[i];
 		i++;
 	}
+	// lst->value[i] = '\0';
+	printf("%s\n", lst->value);
 }
+
+void	dup_key(char *str, t_env *lst)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[j] && str[j] != '=')
+		j++;
+	lst->key = malloc(i + 1);
+	i = 0;
+	// exit(1);
+	while (str && i < j)
+	{
+		lst->key[i] = str[i];
+		i++;
+	}
+	lst->key[i] = '\0';
+	printf("%s\n", lst->key);
+	
+}
+
+// void	split_byequals(t_env *env, char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	// while(str[i] && str[i] != '=')
+// 	// {
+// 		env->key = ft_strdup(str - ft_strlen(str));
+// 	// }
+// 		printf("%s\n", env->key);
+// 		exit(1);
+// 	while(str[i])
+// 	{
+// 		env->value = ft_strdup(str + ft_strlen(str));
+// 		i++;
+// 	}
+// }
 
 // void	export_arg(char *av)
 // {	
@@ -49,9 +95,12 @@ void	my_export(t_env **env, char **av)
 	{
 		ft_lstadd_back(env, ft_lstnew(1));
 		*env = ft_lstlast(*env);
-		split_byequals(*env, av[i]);
+		// split_byequals(*env, av[i]);
+		dup_key(av[i], *env);
+		dup_value(av[i], *env);
 		i++;
 	}
+	exit(1);
 	while (tmp)
 	{
 		printf("%s\n", tmp->key);
