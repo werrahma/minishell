d81079677f88418ed935c1e205bb7f10733ac87e
Walid Errahmani <werrahma@e1r6p3.1337.ma>
2023-05-24 14:41:30 +0100

    fixing multipel pipe but a need to check it

--- execution/pipex.c
+++ execution/pipex.c
@@ -6,7 +6,7 @@
 /*   By: werrahma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 13:15:07 by werrahma          #+#    #+#             */
-/*   Updated: 2023/05/23 19:44:02 by werrahma         ###   ########.fr       */
+/*   Updated: 2023/05/24 14:40:50 by werrahma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,8 @@ void	pipex(t_mini *list, char **env, t_pipe *pipes)
 	int a;
 
 	a = 0;
-	// pipe(pipes->fd[0]);
-	// pipe(pipes->fd[1]);
-	// if (a > 0)
-	// {
-	// 	printf("aaa\n");
-	// 	dup2(pipes->fd[pipes->f0][0], 0);
-	// 	// dup2(list->outfile, 1);
-	// 	execve("/usr/bin/cat", list->cmd, env);
-	// 		close(pipes->fd[pipes->f0][0]);
-	// close(pipes->fd[pipes->f0][1]);
-	// close(pipes->fd[pipes->f1][0]);
-	// close(pipes->fd[pipes->f1][1]);
-	// 	return ;
-	// }
 	flag = 0;
-	// pipes->f0 = 0;
-	// pipes->f1 = 1;
 	int i = 0;
-	// while()
-	// printf("in  %d    out   %d\n", list->infile, list->outfile);
-	// while(list->cmd[i])
-	// 	printf("%s\n", list->cmd[i++]);
-	// return ;
 	if(list->infile > 2)
 	{
 		a++;
@@ -63,39 +42,31 @@ void	pipex(t_mini *list, char **env, t_pipe *pipes)
 	}
 	if (a > 0)
 	{
-		// printf("%d\n", pipes->stdiin);
-		// printf("get == %s\n", get_next_line(pipes->fd[0][0]));
 		printf("am here\n");
 		dup2(pipes->fd[pipes->f0][0], pipes->stdiin);
 	}
-	else if (list->infile < 2 && list->next)
+	else if (list->infile < 2 && list->next && list->outfile < 2)
 	{
 		printf("multiple\n");
 		multiple_pipe(list, env, pipes);
 	}
 	if (!list->next && list->outfile < 2)
 	{
-		// printf("am here\n");
+		printf("am here for flag\n");
 		flag = 1;
 		list->outfile = 1;
 	}
 	if (list->outfile > 2 || flag == 1)
 	{
-		// dprintf (2, "^^^%s\n", get_next_line(pipes->fd[0][1]));
 		printf("i have outfile\n");
 		chld_t = fork();
 		if (chld_t == 0)
 			last_child(list, env, pipes);
 	}
-	// dprintf(2, "$$$$$$$$$$%s/n", get_next_line(pipes->fd[0][0]));
-	// close(pipes->fd[0][0]);
-	// close(pipes->fd[0][1]);
 	close(pipes->fd[pipes->f0][0]);
 	close(pipes->fd[pipes->f0][1]);
 	close(pipes->fd[pipes->f1][0]);
 	close(pipes->fd[pipes->f1][1]);
 	while (wait(NULL) != -1)
 		continue;
-
-	// return ;
 }
