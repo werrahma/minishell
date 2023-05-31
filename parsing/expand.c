3551dbbeb45ed63b55881f2d49e7812db48eb432
Yassine Hamdani <yahamdan@e1r7p6.1337.ma>
2023-05-31 11:22:24 +0100

    fasi

--- parsing/expand.c
+++ parsing/expand.c
@@ -38,7 +38,7 @@ char    *expand_tokens(t_tokens *token, t_env *env)
 	{
 		if(token->cont[i] == '$' &&  token->cont[i + 1] == '?' && lexer_openqts(token->cont, i) != 2)
 		{
-			i ++;
+			i++;
 			str = ft_strjoin(str, ft_itoa(stx));
 		}
 		else if(token->cont[i] == '$' && lexer_openqts(token->cont, i) != 2)
